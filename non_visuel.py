fd6cf4e9c907777a6a99f12069199261e4c23f47
Multigriffes <90653170+Multigriffes@users.noreply.github.com>
2025-03-14 22:40:30 +0100

    Amelioration de la lisibilité, de commentaires, d'un moyen de quitter

--- non_visuel.py
+++ non_visuel.py
@@ -1,6 +1,5 @@
 from random import randint
 
-isplay=True
 map1=([("1011"),("0110"),("1011"),("0011"),("0110")],
 [("1010"),("0101"),("1110"),("1110"),("1100")],
 [("1001"),("0010"),("0000"),("0001"),("0101")],
@@ -23,26 +22,33 @@ def creation_grille_joueur(taille_grille:int=5,pos_joueur:list=[]):
         assert pos_joueur[0]>=taille_grille or pos_joueur[1]>=taille_grille;"les positions données sont en dehors du terrain"
     grille_joueur=[["*" for b in range(taille_grille)] for a in range(taille_grille)]
     grille_joueur[pos_joueur[0]][pos_joueur[1]]="O"
+
     grille_mures=map1 #à gérer pour changement de map
+    
     return [grille_joueur,grille_mures,pos_joueur]
 
 
 #Déplacement
 def mouvement(direction:str,grille_joueur,grille_mures,pos_joueur,nbr_cases_touche,nbr_etoiles_touche):
     
+    #Droite
     if direction=="d" or direction=="droite":
-        if grille_mures[pos_joueur[0]][pos_joueur[1]][1]=="0":
+        if grille_mures[pos_joueur[0]][pos_joueur[1]][1]=="0": #Vérification de la présence d'un mur
+            #MàJ de la position
             grille_joueur[pos_joueur[0]][pos_joueur[1]]=""
             pos_joueur[1]+=1
+            #Ajout du score si étoile
             if grille_joueur[pos_joueur[0]][pos_joueur[1]]=="*":
                 nbr_etoiles_touche+=1
+            #MàJ de la position
             grille_joueur[pos_joueur[0]][pos_joueur[1]]="O"
             return [grille_joueur,pos_joueur,nbr_cases_touche,nbr_etoiles_touche]
         else:
             print("C'est un mur !!! p'tite merde d")
             nbr_cases_touche+=1
             return [grille_joueur,pos_joueur,nbr_cases_touche,nbr_etoiles_touche]
-
+    
+    #Gauche
     elif direction=="g" or direction=="gauche":
         if grille_mures[pos_joueur[0]][pos_joueur[1]][0]=="0":
             grille_joueur[pos_joueur[0]][pos_joueur[1]]=""
@@ -56,6 +62,7 @@ def mouvement(direction:str,grille_joueur,grille_mures,pos_joueur,nbr_cases_touc
             nbr_cases_touche+=1
             return [grille_joueur,pos_joueur,nbr_cases_touche,nbr_etoiles_touche]
     
+    #Bas
     elif direction=="b" or direction=="bas":
         if grille_mures[pos_joueur[0]][pos_joueur[1]][3]=="0":
             grille_joueur[pos_joueur[0]][pos_joueur[1]]=""
@@ -69,6 +76,7 @@ def mouvement(direction:str,grille_joueur,grille_mures,pos_joueur,nbr_cases_touc
             nbr_cases_touche+=1
             return [grille_joueur,pos_joueur,nbr_cases_touche,nbr_etoiles_touche]
     
+    #Haut
     elif direction=="h" or direction=="haut":
         if grille_mures[pos_joueur[0]][pos_joueur[1]][2]=="0":
             grille_joueur[pos_joueur[0]][pos_joueur[1]]=""
@@ -82,42 +90,48 @@ def mouvement(direction:str,grille_joueur,grille_mures,pos_joueur,nbr_cases_touc
             nbr_cases_touche+=1
             return [grille_joueur,pos_joueur,nbr_cases_touche,nbr_etoiles_touche]
     
+    #Non reconnue
     else:
         print("La direction n'est pas reconnu")
         return [grille_joueur,pos_joueur,nbr_cases_touche,nbr_etoiles_touche]
 
 
 def play():
-    #Instruction :
-    #Init :
+    #Initialisation :
+    isplay=True
     nbr_etoiles_touche=0
     nbr_cases_touche=0
     taille_grille=int(input("Taille de la grille souhaitée : "))
     pos_joueur=list(input("Position du joueur initiale si souhaité sinon laisser vide : "))
-    #MàJ variables globales
     info_init=creation_grille_joueur(taille_grille,pos_joueur)
     grille_joueur=info_init[0]
     grille_mures=info_init[1]
     pos_joueur=info_init[2]
-    #Boucle de jeu/déplacement
-    while isplay:
+
+    #Boucle de jeu
+    while isplay==True:
+        #Affichage
         for i in range(taille_grille):
             print(grille_joueur[i])
         for i in range(taille_grille):
             print(grille_mures[i])
+        
+        #Input du joueur
         direction=input("Direction souhaitée : ")
-
-        #Debug
-        if direction=="u":
-            print(grille_mures[pos_joueur[0]][pos_joueur[1]][0],grille_mures[pos_joueur[0]][pos_joueur[1]][1],grille_mures[pos_joueur[0]][pos_joueur[1]][2],grille_mures[pos_joueur[0]][pos_joueur[1]][3])
-            print(nbr_cases_touche,nbr_etoiles_touche)
-
-        #MàJ variables globales
+        
+        #Déplacement et MàJ des variables
         info_mouv=mouvement(direction,grille_joueur,grille_mures,pos_joueur,nbr_cases_touche,nbr_etoiles_touche)
         grille_joueur=info_mouv[0]
         pos_joueur=info_mouv[1]
         nbr_cases_touche=info_mouv[2]
         nbr_etoiles_touche=info_mouv[3]
+    
+        #Debug
+        if direction=="u":
+            print(grille_mures[pos_joueur[0]][pos_joueur[1]][0],grille_mures[pos_joueur[0]][pos_joueur[1]][1],grille_mures[pos_joueur[0]][pos_joueur[1]][2],grille_mures[pos_joueur[0]][pos_joueur[1]][3])
+            print(nbr_cases_touche,nbr_etoiles_touche)
+        elif direction=="q" or direction=="quitte" or direction=="quit":
+            isplay=False
 
 
 play()
