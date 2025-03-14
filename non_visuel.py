60d86a5e6309aa6f66b4e3f9f1141be0a326872e
Multigriffes <90653170+Multigriffes@users.noreply.github.com>
2025-03-14 14:31:43 +0100

    rien

--- non_visuel.py
+++ non_visuel.py
@@ -1,4 +1,7 @@
 from random import randint
 
 def creation_grille_joueur(taille_grille:int=5,pos_joueur:list=[]):
-    assert 
+    assert type(taille_grille)==int ;"la taille de la grille n'est pas un int"
+    if pos_joueur==[]:
+        pos_joueur=[randint(0,taille_grille),randint(0,taille_grille)]
+    grille_joueur=
