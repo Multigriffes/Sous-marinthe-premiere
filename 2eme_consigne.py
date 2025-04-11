afa90e83274ac5c406a9b640b6831f5c99bc9c73
Multigriffes <jonas.t@laposte.net>
2025-04-11 15:01:06 +0200

    Début pygame

--- 2eme_consigne.py
+++ 2eme_consigne.py
@@ -1,19 +1,28 @@
 from random import randint
 from generation_liste_maps import generation_liste
+import pygame
 
 liste_map=generation_liste()
+color={"blanc":(255,255,255),"noir":(0,0,0)}
+
+sous_marin=pygame.image.load("img/sousmarin.png")
+etoile=pygame.image.load("img/etoile.png")
+sortie=pygame.image.load("img/porte.png")
+background=pygame.Surface((550,550))
+background.fill(color["blanc"])
+
 
 #Creation des grilles
 def creation_grille_joueur(taille_grille:int,pos_joueur:list=[]):
     """
     Cette fonction créé la liste représentant la grille et représente le joueur dessus à l'aide 
-    d'un "O" et les cases vides avec un "*"
+    d'un "O" et les cases vides avec un "*" et la sorite avec un "S"
     """
     
-    assert type(taille_grille)==int ;"la taille de la grille n'est pas un int"
+    assert type(taille_grille)==int ,"la taille de la grille n'est pas un int"
     assert taille_grille>1, "Grille trop petite"
-    assert taille_grille<=15 ;"On a pas autant de map que ça, 15 pas plus"
-    assert type(pos_joueur)==list ;"la position du joueur n'est pas une liste"
+    assert taille_grille<=15 ,"On a pas autant de map que ça, 15 pas plus"
+    assert type(pos_joueur)==list ,"la position du joueur n'est pas une liste"
 
 
     #Génération ou vérification des coordonnée du joueur
@@ -26,17 +35,25 @@ def creation_grille_joueur(taille_grille:int,pos_joueur:list=[]):
             pos_joueur=[]
     if pos_joueur == []:
         pos_joueur = [randint(0,taille_grille-1), randint(0,taille_grille-1)]
+    pos_joueur_init=pos_joueur.copy()
+    
+    #Génération des coordonnée de la sortie
+    pos_sortie=pos_joueur
+    while pos_joueur==pos_sortie:
+        pos_sortie=[randint(0,taille_grille-1),randint(0,taille_grille-1)]
 
     #Création de la grille du joueur
     grille_joueur = [["*" for i in range(taille_grille)] for b in range(taille_grille)]
     grille_joueur[pos_joueur[1]][pos_joueur[0]] = "O"
+    grille_joueur[pos_sortie[1]][pos_sortie[0]] = "S"
+
 
     #Choix de le map en fonction du nbr de cases
     grille_murs = liste_map[taille_grille-2][randint(0,len(liste_map[taille_grille-2])-1)]
 
-    return [grille_joueur,grille_murs,pos_joueur]
+    return [grille_joueur,grille_murs,pos_joueur,pos_joueur_init,pos_sortie]
 
-def action(commande:str,grille_joueur:list,grille_murs:list,pos_joueur:list,nbr_etoiles:int,nbr_murs:int):
+def action(commande:str,grille_joueur:list,grille_murs:list,pos_joueur:list,pos_joueur_init:list,pos_sortie:list,nbr_murs:int,screen):
     """
     La fonction gère les action du joueur et sont influence sur les diffèrentes variables du jeu
     """
@@ -45,59 +62,63 @@ def action(commande:str,grille_joueur:list,grille_murs:list,pos_joueur:list,nbr_
     assert type(grille_joueur) == list, "grille_joueur n'est pas un list"
     assert type(grille_murs) == tuple, "grille_murs n'est pas un tuple"
     assert type(pos_joueur) == list, "pos_joueur n'est pas un list"
-    assert type(nbr_etoiles) == int, "nbr_etoiles n'est pas un int"
+    assert type(pos_joueur_init) == list, "pos_joueur_init n'est pas un list"
+    assert type(pos_sortie) == list, "pos_sortie n'est pas un list"
     assert type(nbr_murs) == int, "nbr_murs n'est pas un int"
     
     for j in commande:
-        grille_joueur[pos_joueur[1]][pos_joueur[0]]=" "
+        grille_joueur[pos_joueur[1]][pos_joueur[0]]="*"
         if j=="d":
             if grille_murs[pos_joueur[1]][pos_joueur[0]][1]=="0":
                 pos_joueur[0]+=1
-                if grille_joueur[pos_joueur[1]][pos_joueur[0]]=="*":
-                    nbr_etoiles+=1
             else:
-                print("C'est un MUR CHEHHHHH !!!!!!!!!!")
                 nbr_murs+=1
+                print("C'est un MUR CHEHHHHH !!!!!!!!!!")
+                pos_joueur=pos_joueur_init.copy()
         elif j=="g" or j=="q":
             if grille_murs[pos_joueur[1]][pos_joueur[0]][0]=="0":
                 pos_joueur[0]-=1
-                if grille_joueur[pos_joueur[1]][pos_joueur[0]]=="*":
-                    nbr_etoiles+=1
             else:
-                print("C'est un MUR CHEHHHHH !!!!!!!!!!")
                 nbr_murs+=1
+                print("C'est un MUR CHEHHHHH !!!!!!!!!!")
+                pos_joueur=pos_joueur_init.copy()
         elif j=="h" or j=="z":
             if grille_murs[pos_joueur[1]][pos_joueur[0]][2]=="0":
                 pos_joueur[1]-=1
-                if grille_joueur[pos_joueur[1]][pos_joueur[0]]=="*":
-                    nbr_etoiles+=1
             else:
-                print("C'est un MUR CHEHHHHH !!!!!!!!!!")
                 nbr_murs+=1
+                print("C'est un MUR CHEHHHHH !!!!!!!!!!")
+                pos_joueur=pos_joueur_init.copy()
         elif j=="b" or j=="s":
             if grille_murs[pos_joueur[1]][pos_joueur[0]][3]=="0":
                 pos_joueur[1]+=1
-                if grille_joueur[pos_joueur[1]][pos_joueur[0]]=="*":
-                    nbr_etoiles+=1
             else:
-                print("C'est un MUR CHEHHHHH !!!!!!!!!!")
                 nbr_murs+=1
+                print("C'est un MUR CHEHHHHH !!!!!!!!!!")
+                pos_joueur=pos_joueur_init.copy()
         else:
             print(j+" : La commande n'est pas reconnu")
     grille_joueur[pos_joueur[1]][pos_joueur[0]]="O"
-    return [grille_joueur,pos_joueur,nbr_etoiles,nbr_murs]
-
-def affichage(grille_joueur,nbr_etoiles,nbr_murs):
+    grille_joueur[pos_sortie[1]][pos_sortie[0]]="S"
+    return [grille_joueur,pos_joueur,nbr_murs,screen]
+    
+def affichage(grille_joueur,nbr_murs,screen):
 
     assert type(grille_joueur)==list, "grille_joueur n'est pas une liste"
-    assert type(nbr_etoiles)==int, "nbr_etoiles n'est pas un int"
     assert type(nbr_murs)==int, "nbr_murs n'est pas un int"
 
-    #PYGAME
+    screen.blit(background,(0,0))
+
+
+    pygame.display.flip()
+
+    return screen
+
+
 
 def play():
     #Initialisation avec input joueur
-    nbr_etoiles,nbr_murs,isPlay,taille_grille,pos_joueur=0,0,True,"",['default']
+    nbr_murs,isPlay,taille_grille,pos_joueur=0,True,"",['default']
 
     while taille_grille=="":
         taille_grille=input("Taille de la grille souhaitée : ")
@@ -146,23 +167,38 @@ def play():
                 pos_joueur_temp[i]=int(pos_joueur_temp[i])
             pos_joueur=pos_joueur_temp.copy()
 
-    grille_joueur,grille_murs,pos_joueur=creation_grille_joueur(taille_grille,pos_joueur)
+    grille_joueur,grille_murs,pos_joueur,pos_joueur_init,pos_sortie,screen=creation_grille_joueur(taille_grille,pos_joueur)
+
+    pygame.init()
+    screen=pygame.display.set_mode((750,550))
+    screen.blit(background,(0,0))
+    pygame.display.flip()
 
     while isPlay:
-        affichage(grille_joueur,nbr_etoiles,nbr_murs)
-        
-        #Instructions de jeu et mise à jour des variables
+        screen=affichage(grille_joueur,nbr_murs,screen)
+        pygame.time.wait(500)
         commande=""
         while commande=="":
-            commande=str(input("Action souhaitée : ")).lower()
-        
-        if commande=="exit":
-            isPlay=False
-        else:
-            grille_joueur,pos_joueur,nbr_etoiles,nbr_murs=action(commande,grille_joueur,grille_murs,pos_joueur,nbr_etoiles,nbr_murs) # type: ignore
+            for event in pygame.event.get():
+                if event.type==pygame.QUIT:
+                    isPlay=False
+                    pygame.quit()
+                if event.type==pygame.KEYDOWN:
+                    if event.key==pygame.K_z or event.key==pygame.K_UP:
+                        commande="z"
+                    if event.key==pygame.K_s or event.key==pygame.K_DOWN:
+                        commande="s"
+                    if event.key==pygame.K_d or event.key==pygame.K_RIGHT:
+                        commande="d"
+                    if event.key==pygame.K_q or event.key==pygame.K_LEFT:
+                        commande="q"
+                if event.type==pygame.KEYUP:
+                    commande=""
 
+        grille_joueur,pos_joueur,nbr_murs,screen=action(commande,grille_joueur,grille_murs,pos_joueur,pos_joueur_init,pos_sortie,nbr_murs,screen) # type: ignore
 
-        if nbr_etoiles==taille_grille**2-1:
-            print("Bien joué, tu as touché",nbr_murs,"murs et attrapé",nbr_etoiles,"étoiles. GG ou pas")
+        if pos_joueur==pos_sortie:
+            print("Bien joué, tu as recommencé",nbr_murs,"fois avant de gagner. GG ou pas")
+            pygame.time.wait(10000)
             isPlay=False
-        
+            pygame.quit()
