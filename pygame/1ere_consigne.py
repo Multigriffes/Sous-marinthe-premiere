76cabc225436a796f3547419f168fb6a97827b9d
Multigriffes <90653170+Multigriffes@users.noreply.github.com>
2025-07-02 21:53:08 +0200

    Migration (#2)
    * Début du pygame
    * Update README.md
    * Optimisation de la mise à jour des variables
    * Update .gitignore
    * Ajout des extensions recommandées
    * Début pygame
    * Exit la doc .txt lol
    * Maj de la doc en conséquence
    * Séparation du traitement des inputs initiaux
    * Opti de stockage + typo
    * Create background.jpg
    * son
    * son correction
    * fix son plus sync des deux consignes
    * Avance pygame
    * Update victoire.png
    * typo
    * Delete Sous-marinthe.docx
    * Update sous_marinthe.py
    * Ajout des fichier pour migration
    
    ---------
    
    Co-authored-by: Baptiste <rigollet.baptiste@gmail.com>

---
+++ pygame/1ere_consigne.py
@@ -0,0 +1,283 @@
+from random import randint
+from generation_liste_maps import generation_liste
+import pygame
+
+pygame.font.init()
+pygame.mixer.init()
+
+#J'ai fait une régression qui ma donner que chaque taille de font compte pour 0.6805789px pour le texte "Wasted"
+#Et que la largeur est 3.58 x la hauteur
+
+bop = pygame.mixer.Sound("sound/pop-331049.mp3")
+
+screenXY=(1920,1080)
+windowXY=(0.6*screenXY[0],0.6*screenXY[1])
+background_labyrintheXY=(0.86*windowXY[0],0.97*windowXY[1])
+background_scoreXY=(0.1*windowXY[0],0.97*windowXY[1])
+background_wastedXY=(windowXY[0],0.25*windowXY[1])
+text_wastedXY=(0.98*background_wastedXY[1]*3.58,0.98*background_wastedXY[1])
+victory_bannerXY=(0.8*windowXY[0], 0.8*windowXY[1])
+
+taille_font_wasted=text_wastedXY[1]//0.6805789
+
+pos_background_labyrinthe=(0.0156*windowXY[0],0.0139*windowXY[1])
+pos_background_score=(0.89*windowXY[0],0.0139*windowXY[1])
+pos_background_wasted=(0,windowXY[1]/2-background_wastedXY[1]/2)
+pos_text_wasted=(windowXY[0]/2-text_wastedXY[0]/2,windowXY[1]/2-text_wastedXY[1]/2)
+pos_victory_banner=(windowXY[0]/2-victory_bannerXY[0]/2, windowXY[1]/2-victory_bannerXY[1]/2)
+
+background=pygame.image.load("img/background.jpg")
+background=pygame.transform.scale(background,windowXY)
+
+victory_banner=pygame.image.load("img/victoire.png")
+victory_banner=pygame.transform.scale(victory_banner,victory_bannerXY)
+
+background_labyrinthe=pygame.Surface(background_labyrintheXY)
+background_labyrinthe.fill((0,0,0))
+
+background_score=pygame.Surface(background_scoreXY)
+background_score.fill((255,255,255))
+
+background_wasted=pygame.Surface(background_wastedXY)
+background_wasted.fill((255,0,0))
+text_wasted_font=pygame.font.SysFont(None,round(text_wastedXY[1]/0.6805789),True)
+text_wasted=pygame.font.Font.render(text_wasted_font,"Wasted",True,(255,255,255))
+
+
+#Creation des grilles
+def creation_grille_joueur(taille_grille:int,pos_joueur:list=[]):
+    """
+    Cette fonction créé la liste représentant la grille et représente le joueur dessus à l'aide 
+    d'un "O" et les cases vides avec un "*"
+    """
+    
+    assert type(taille_grille)==int ,"la taille de la grille n'est pas un int"
+    assert taille_grille>1, "Grille trop petite"
+    assert taille_grille<=15 ,"On a pas autant de map que ça, 15 pas plus"
+    assert type(pos_joueur)==list ,"la position du joueur n'est pas une liste"
+
+
+    #Génération ou vérification des coordonnée du joueur
+    if pos_joueur!=[]:
+        if pos_joueur[0] > taille_grille-1 or pos_joueur[1] > taille_grille-1:
+            print("Position hors du terrain, génération aléatoire...")
+            pos_joueur=[]
+        if len(pos_joueur)!=2:
+            print("Y a 2 nombres pour une coordonnées en 2D idiots, génération aléatoire...")
+            pos_joueur=[]
+    else:
+        pos_joueur = [randint(0,taille_grille-1), randint(0,taille_grille-1)]
+
+    #Création de la grille du joueur
+    grille_joueur = [["*" for i in range(taille_grille)] for b in range(taille_grille)]
+    grille_joueur[pos_joueur[1]][pos_joueur[0]] = "O"
+
+    #Choix de le map en fonction du nbr de cases
+    grille_murs = generation_liste()[taille_grille-2][randint(0,len(generation_liste()[taille_grille-2])-1)]
+
+    return [grille_joueur,grille_murs,pos_joueur]
+
+def action(commande:str,grille_joueur:list,grille_murs:list,pos_joueur:list,nbr_etoiles:int,nbr_murs:int,screen):
+    """
+    La fonction gère les action du joueur et sont influence sur les diffèrentes variables du jeu
+    """
+
+    assert type(commande) == str, "commande n'est pas un string"
+    assert type(grille_joueur) == list, "grille_joueur n'est pas un list"
+    assert type(grille_murs) == tuple, "grille_murs n'est pas un tuple"
+    assert type(pos_joueur) == list, "pos_joueur n'est pas un list"
+    assert type(nbr_etoiles) == int, "nbr_etoiles n'est pas un int"
+    assert type(nbr_murs) == int, "nbr_murs n'est pas un int"
+    bop.play(loops=0, maxtime=0, fade_ms=0)
+
+    
+    for j in commande:
+        grille_joueur[pos_joueur[1]][pos_joueur[0]]=" "
+        if j=="d":
+            if grille_murs[pos_joueur[1]][pos_joueur[0]][1]=="0":
+                pos_joueur[0]+=1
+                if grille_joueur[pos_joueur[1]][pos_joueur[0]]=="*":
+                    nbr_etoiles+=1
+            else:
+                nbr_murs+=1
+                screen=affichage_wasted(screen)
+        elif j=="q":
+            if grille_murs[pos_joueur[1]][pos_joueur[0]][0]=="0":
+                pos_joueur[0]-=1
+                if grille_joueur[pos_joueur[1]][pos_joueur[0]]=="*":
+                    nbr_etoiles+=1
+            else:
+                nbr_murs+=1
+                screen=affichage_wasted(screen)
+        elif j=="z":
+            if grille_murs[pos_joueur[1]][pos_joueur[0]][2]=="0":
+                pos_joueur[1]-=1
+                if grille_joueur[pos_joueur[1]][pos_joueur[0]]=="*":
+                    nbr_etoiles+=1
+            else:
+                nbr_murs+=1
+                screen=affichage_wasted(screen)
+        elif j=="s":
+            if grille_murs[pos_joueur[1]][pos_joueur[0]][3]=="0":
+                pos_joueur[1]+=1
+                if grille_joueur[pos_joueur[1]][pos_joueur[0]]=="*":
+                    nbr_etoiles+=1
+            else:
+                nbr_murs+=1
+                screen=affichage_wasted(screen)
+        else:
+            print(j+" : La commande n'est pas reconnu")
+    grille_joueur[pos_joueur[1]][pos_joueur[0]]="O"
+    return [grille_joueur,pos_joueur,nbr_etoiles,nbr_murs,screen]
+
+def affichage_wasted(screen):
+    screen.blit(background_wasted,pos_background_wasted)
+    screen.blit(text_wasted,pos_text_wasted)
+    pygame.display.flip()
+    pygame.time.wait(1000)
+    return screen
+
+def affichage(grille_joueur,nbr_etoiles,nbr_murs,screen,caseXY,sous_marin,etoile,case):
+
+    assert type(grille_joueur)==list, "grille_joueur n'est pas une liste"
+    assert type(nbr_etoiles)==int, "nbr_etoiles n'est pas un int"
+    assert type(nbr_murs)==int, "nbr_murs n'est pas un int"
+
+    screen.blit(background,(0,0))
+    screen.blit(background_labyrinthe,pos_background_labyrinthe)
+    screen.blit(background_score,pos_background_score)
+    for y in range(len(grille_joueur)):
+        for x in range(len(grille_joueur)):
+            screen.blit(case,(pos_background_labyrinthe[0]+(x+0.05)*caseXY[0],pos_background_labyrinthe[1]+(y+0.05)*caseXY[1]))
+            if grille_joueur[y][x]=='O':
+                screen.blit(sous_marin,(pos_background_labyrinthe[0]+(x+0.05)*caseXY[0],pos_background_labyrinthe[1]+(y+0.05)*caseXY[1]))
+            elif grille_joueur[y][x]=='*':
+                screen.blit(etoile,(pos_background_labyrinthe[0]+(x+0.05)*caseXY[0],pos_background_labyrinthe[1]+(y+0.05)*caseXY[1]))
+
+    pygame.display.flip()
+
+    return screen
+
+def input_taille_grille():
+    '''
+    La fonction va gérer la demande de l'input du joueur pour la taille de la grille
+    '''
+    taille_grille=""
+    while taille_grille=="":
+        taille_grille=input("Taille de la grille souhaitée entre 2 et 15 : ")
+        try:
+            int(taille_grille)
+        except:
+            taille_grille=""
+        else:
+            taille_grille=int(taille_grille)
+            if taille_grille>15 or taille_grille<2:
+                taille_grille=""
+    return taille_grille
+
+def input_pos_joueur():
+    '''
+    La fonction va gérer la demande de l'input joueur pour la position du joueur
+    '''
+    pos_joueur=['default']
+    while pos_joueur==['default']:
+        pos_joueur=list(input("Position du joueur initiale si souhaité sinon laisser vide : "))
+        #Nettoyage de la liste de string de la position
+        if pos_joueur!=['default'] and pos_joueur!=[]:#Vérification du besoin de nettoyer la liste
+            a=0
+            for i in range(len(pos_joueur)):#Suppression de tout ce qui n'est pas un chiffre ou une virguel
+                try:
+                    int(pos_joueur[a])
+                except:
+                    if pos_joueur[a]==',':
+                        a+=1
+                    else:
+                        del pos_joueur[a]
+                else:
+                    a+=1
+            pos_joueur_temp,isVirgule,a=[],False,0
+            for i in pos_joueur:#Rassemblement des potentiels nombre séparés par des virgules
+                if i==',':
+                    isVirgule=True
+                else:
+                    try:
+                        pos_joueur_temp[a-1]
+                    except:
+                        pos_joueur_temp.append(i)
+                        a+=1
+                    else:
+                        if isVirgule:
+                            pos_joueur_temp.append(i)
+                            a+=1
+                        else:
+                            pos_joueur_temp[a-1]=pos_joueur_temp[a-1]+i
+                    isVirgule=False
+            for i in range(len(pos_joueur_temp)):#Transformation des str de nombre en int
+                pos_joueur_temp[i]=int(pos_joueur_temp[i])
+            pos_joueur=pos_joueur_temp.copy()
+    return pos_joueur
+
+def play():
+    '''
+    Fonction pour le lancement du jeu, initialisation des variables puis boucle de jeu
+    '''
+    #Initialisation avec input joueur
+    nbr_etoiles,nbr_murs,isPlay=0,0,True
+    sous_marin=pygame.image.load("img/sousmarin.png")
+    etoile=pygame.image.load("img/etoile.png")
+
+    taille_grille=input_taille_grille()
+    caseXY=(background_labyrintheXY[0]/taille_grille,background_labyrintheXY[1]/taille_grille)
+    case=pygame.Surface((caseXY[0]*0.9,caseXY[1]*0.9))
+    case.fill((255,255,255))
+    sous_marin=pygame.transform.scale(sous_marin,(caseXY[0]*0.9,caseXY[1]*0.9))
+    etoile=pygame.transform.scale(etoile,(caseXY[0]*0.9,caseXY[1]*0.9))
+
+    pos_joueur=input_pos_joueur()
+
+    grille_joueur,grille_murs,pos_joueur=creation_grille_joueur(taille_grille,pos_joueur)
+
+    pygame.init()
+    screen=pygame.display.set_mode(windowXY)
+
+    #Gestion du son/musique :
+    son = pygame.mixer.Sound('sound/epical-music-background-337255.mp3')
+    son.play(loops=-1, maxtime=0, fade_ms=0)
+
+    while isPlay:#Boucle de jeu
+        screen=affichage(grille_joueur,nbr_etoiles,nbr_murs,screen,caseXY,sous_marin,etoile,case)
+        pygame.time.wait(50)
+        commande=""
+        while commande=="":
+            for event in pygame.event.get():
+                if event.type==pygame.QUIT:
+                    isPlay=False
+                    commande='quit'
+                if event.type==pygame.KEYDOWN:
+                    if event.key==pygame.K_z or event.key==pygame.K_UP:
+                        commande="z"
+                    if event.key==pygame.K_s or event.key==pygame.K_DOWN:
+                        commande="s"
+                    if event.key==pygame.K_d or event.key==pygame.K_RIGHT:
+                        commande="d"
+                    if event.key==pygame.K_q or event.key==pygame.K_LEFT:
+                        commande="q"
+                if event.type==pygame.KEYUP:
+                    commande=""
+        if commande!='quit':
+            grille_joueur,pos_joueur,nbr_etoiles,nbr_murs,screen=action(commande,grille_joueur,grille_murs,pos_joueur,nbr_etoiles,nbr_murs,screen) # type: ignore
+        else:
+            pygame.quit()
+        if nbr_etoiles==taille_grille**2-1:
+            screen=affichage(grille_joueur,nbr_etoiles,nbr_murs,screen,caseXY,sous_marin,etoile,case)
+            screen.blit(victory_banner, pos_victory_banner)
+
+            pygame.mixer.Sound.stop(son)
+            son_victory = pygame.mixer.Sound('sound/Voicy_macarena.mp3')
+            son_victory.play(loops=0, maxtime=0, fade_ms=0)
+
+            pygame.display.flip()
+            print("Bien joué, tu as touché",nbr_murs,"murs et attrapé",nbr_etoiles,"étoiles. GG ou pas")
+            pygame.time.wait(20000)
+            isPlay=False
+            pygame.quit()
---
+++ pygame/sous_marinthe.py
@@ -0,0 +1,264 @@
+from random import randint
+from generation_liste_maps import generation_liste
+import pygame
+pygame.init()
+
+
+# 0=menu selection mode de jeu
+# 1=menu selection taille mode de jeu 1
+# 2=menu selection pos mode de jeu 1
+# 3=menu selection taille mode de jeu 2
+# 4=menu selection pos mode de jeu 2
+
+
+def affichage(window,etape):
+    if etape==0:
+        window=affichage_menu_mode_jeu(window)
+    elif etape==1:
+        window=affichage_menu_taille_grille(window)
+    elif etape==3:
+        window=affichage_menu_taille_grille(window)
+
+    return window
+
+
+
+def size_computing_menu_mode_jeu(window):
+# ____________________________________________________________
+    # Création des textes
+    title=title_font.render("Sous-Marinthe",False,(255, 122, 33))
+    text_mode_jeu_1=title_font.render("Collecte toutes les étoiles",False,(0,0,0))
+    text_mode_jeu_2=title_font.render("Atteint la porte",False,(0,0,0))
+    text_createurs=title_font.render("Jonas Tourvieille, Baptiste Rigollet",False,(0,0,0))
+# ____________________________________________________________
+    # Taille des surfaces
+    windowXY=window.get_size()
+    backgroundXY=(windowXY[0]*1,windowXY[1]*1)
+    titleXY=(backgroundXY[0]*0.8,backgroundXY[1]*0.3)
+    background_mode_jeuXY=(backgroundXY[0]*0.3,backgroundXY[1]*0.5)
+    icon_mode_jeuXY=(0.775*background_mode_jeuXY[0],0.7*background_mode_jeuXY[1])
+    text_mode_jeuXY=(0.8*background_mode_jeuXY[0],0.22*background_mode_jeuXY[1])
+    text_createursXY=(0.4*backgroundXY[0],backgroundXY[1]*0.05)
+
+# ____________________________________________________________
+    # Création des surfaces pleine
+    background_mode_jeu=pygame.Surface(background_mode_jeuXY)
+    background_mode_jeu.fill((110, 105, 92))
+
+# ____________________________________________________________
+    # Scaling des élèments
+    background=pygame.transform.scale(background_image,backgroundXY)
+    title=pygame.transform.scale(title,titleXY)
+    icon_mode_jeu_1=pygame.transform.scale(etoile_image,icon_mode_jeuXY)
+    icon_mode_jeu_2=pygame.transform.scale(porte_image,icon_mode_jeuXY)
+    text_mode_jeu_1=pygame.transform.scale(text_mode_jeu_1,text_mode_jeuXY)
+    text_mode_jeu_2=pygame.transform.scale(text_mode_jeu_2,text_mode_jeuXY)
+    text_createurs=pygame.transform.scale(text_createurs,text_createursXY)
+
+    
+# ____________________________________________________________
+    # Position des élèments
+    background_pos=(0,0)
+    title_pos=(backgroundXY[0]/2-titleXY[0]/2,backgroundXY[1]*0.03)
+    background_mode_jeu_1_pos=(backgroundXY[0]*0.13,backgroundXY[1]*0.35)
+    background_mode_jeu_2_pos=(backgroundXY[0]-backgroundXY[0]*0.13-background_mode_jeuXY[0],backgroundXY[1]*0.35)
+    icon_mode_jeu_1_pos=(background_mode_jeu_1_pos[0]+background_mode_jeuXY[0]/2-icon_mode_jeuXY[0]/2,background_mode_jeu_1_pos[1]+background_mode_jeuXY[1]*0.01)
+    icon_mode_jeu_2_pos=(background_mode_jeu_2_pos[0]+background_mode_jeuXY[0]/2-icon_mode_jeuXY[0]/2,background_mode_jeu_2_pos[1]+background_mode_jeuXY[1]*0.01)
+    text_mode_jeu_1_pos=(background_mode_jeu_1_pos[0]+background_mode_jeuXY[0]/2-text_mode_jeuXY[0]/2,background_mode_jeu_1_pos[1]+background_mode_jeuXY[1]*0.75)
+    text_mode_jeu_2_pos=(background_mode_jeu_2_pos[0]+background_mode_jeuXY[0]/2-text_mode_jeuXY[0]/2,background_mode_jeu_2_pos[1]+background_mode_jeuXY[1]*0.75)
+    text_createurs_pos=(background_mode_jeuXY[0]*0.05,backgroundXY[1]-text_createursXY[1])
+
+# ____________________________________________________________
+
+    return background_mode_jeuXY,background_mode_jeu,background,title,icon_mode_jeu_1,icon_mode_jeu_2,text_mode_jeu_1,text_mode_jeu_2,text_createurs,background_pos,title_pos,background_mode_jeu_1_pos,background_mode_jeu_2_pos,icon_mode_jeu_1_pos,icon_mode_jeu_2_pos,text_mode_jeu_1_pos,text_mode_jeu_2_pos,text_createurs_pos
+
+
+def affichage_menu_mode_jeu(window):
+    background_mode_jeuXY,background_mode_jeu,background,title,icon_mode_jeu_1,icon_mode_jeu_2,text_mode_jeu_1,text_mode_jeu_2,text_createurs,background_pos,title_pos,background_mode_jeu_1_pos,background_mode_jeu_2_pos,icon_mode_jeu_1_pos,icon_mode_jeu_2_pos,text_mode_jeu_1_pos,text_mode_jeu_2_pos,text_createurs_pos=size_computing_menu_mode_jeu(window)
+    
+    window.blit(background,background_pos)
+    window.blit(title,title_pos)
+    if background_mode_jeu_1_pos[0]<pygame.mouse.get_pos()[0]<background_mode_jeu_1_pos[0]+background_mode_jeuXY[0] and background_mode_jeu_1_pos[1]<pygame.mouse.get_pos()[1]<background_mode_jeu_1_pos[1]+background_mode_jeuXY[1]:
+        background_mode_jeu.fill((148, 139, 115))
+        window.blit(background_mode_jeu,background_mode_jeu_1_pos)
+        background_mode_jeu.fill((110, 105, 92))
+    else:
+        window.blit(background_mode_jeu,background_mode_jeu_1_pos)
+    if background_mode_jeu_2_pos[0]<pygame.mouse.get_pos()[0]<background_mode_jeu_2_pos[0]+background_mode_jeuXY[0] and background_mode_jeu_2_pos[1]<pygame.mouse.get_pos()[1]<background_mode_jeu_2_pos[1]+background_mode_jeuXY[1]:
+        background_mode_jeu.fill((148, 139, 115))
+        window.blit(background_mode_jeu,background_mode_jeu_2_pos)
+        background_mode_jeu.fill((110, 105, 92))
+    else:
+        window.blit(background_mode_jeu,background_mode_jeu_2_pos)
+    window.blit(icon_mode_jeu_1,icon_mode_jeu_1_pos)
+    window.blit(icon_mode_jeu_2,icon_mode_jeu_2_pos)
+    window.blit(text_mode_jeu_1,text_mode_jeu_1_pos)
+    window.blit(text_mode_jeu_2,text_mode_jeu_2_pos)
+    window.blit(text_createurs,text_createurs_pos)
+
+    pygame.display.flip()
+    return window
+
+
+def size_computing_menu_taille_grille(window):
+# ____________________________________________________________
+    # Création des textes
+    text_taille_2=title_font.render("2",False,(0,0,0))
+    text_taille_3=title_font.render("3",False,(0,0,0))
+    text_taille_4=title_font.render("4",False,(0,0,0))
+    text_taille_5=title_font.render("5",False,(0,0,0))
+    text_taille_6=title_font.render("6",False,(0,0,0))
+    text_taille_7=title_font.render("7",False,(0,0,0))
+    text_taille_8=title_font.render("8",False,(0,0,0))
+    text_taille_9=title_font.render("9",False,(0,0,0))
+    text_taille_10=title_font.render("10",False,(0,0,0))
+    text_taille_11=title_font.render("11",False,(0,0,0))
+    text_taille_12=title_font.render("12",False,(0,0,0))
+    text_taille_13=title_font.render("13",False,(0,0,0))
+    text_taille_14=title_font.render("14",False,(0,0,0))
+    text_taille_15=title_font.render("15",False,(0,0,0))
+    title_menu_taille_grille=title_font.render("Choisi la taille de la grille",False,(0,0,0))
+    title_menu_pos_grille=title_font.render("Choisi la position de départ",False,(0,0,0))
+# ____________________________________________________________
+    # Taille des surfaces
+    windowXY=window.get_size()
+    backgroundXY=(windowXY[0]*1,windowXY[1]*1)
+    titleXY=(backgroundXY[0]*0.8,backgroundXY[1]*0.15)
+    background_tailleXY=(backgroundXY[0]*0.08,backgroundXY[1]*0.1)
+    text_tailleXY=(1*background_tailleXY[0],1*background_tailleXY[1])
+
+# ____________________________________________________________
+    # Création des surfaces pleine
+
+
+# ____________________________________________________________
+    # Scaling des élèments
+    background=pygame.transform.scale(background_image,backgroundXY)
+    title_menu_taille_grille=pygame.transform.scale(title_menu_taille_grille,titleXY)
+    text_taille_2=pygame.transform.scale(text_taille_2,text_tailleXY)
+    text_taille_3=pygame.transform.scale(text_taille_3,text_tailleXY)
+    text_taille_4=pygame.transform.scale(text_taille_4,text_tailleXY)
+    text_taille_5=pygame.transform.scale(text_taille_5,text_tailleXY)
+    text_taille_6=pygame.transform.scale(text_taille_6,text_tailleXY)
+    text_taille_7=pygame.transform.scale(text_taille_7,text_tailleXY)
+    text_taille_8=pygame.transform.scale(text_taille_8,text_tailleXY)
+    text_taille_9=pygame.transform.scale(text_taille_9,text_tailleXY)
+    text_taille_10=pygame.transform.scale(text_taille_10,text_tailleXY)
+    text_taille_11=pygame.transform.scale(text_taille_11,text_tailleXY)
+    text_taille_12=pygame.transform.scale(text_taille_12,text_tailleXY)
+    text_taille_13=pygame.transform.scale(text_taille_13,text_tailleXY)
+    text_taille_14=pygame.transform.scale(text_taille_14,text_tailleXY)
+    text_taille_15=pygame.transform.scale(text_taille_15,text_tailleXY)
+    
+# ____________________________________________________________
+    # Position des élèments
+    background_pos=(0,0)
+    title_menu_taille_grille_pos=(backgroundXY[0]/2-titleXY[0]/2,backgroundXY[1]*0.03)
+    text_taille_pos=(0.1*backgroundXY[0],0.3*backgroundXY[1])
+
+# ____________________________________________________________
+    return backgroundXY,background,title_menu_taille_grille,text_taille_2,text_taille_3,text_taille_4,text_taille_5,text_taille_6,text_taille_7,text_taille_8,text_taille_9,text_taille_10,text_taille_11,text_taille_12,text_taille_13,text_taille_14,text_taille_15,background_pos,title_menu_taille_grille_pos,text_taille_pos
+
+
+def affichage_menu_taille_grille(window):
+    backgroundXY,background,title_menu_taille_grille,text_taille_2,text_taille_3,text_taille_4,text_taille_5,text_taille_6,text_taille_7,text_taille_8,text_taille_9,text_taille_10,text_taille_11,text_taille_12,text_taille_13,text_taille_14,text_taille_15,background_pos,title_menu_taille_grille_pos,text_taille_pos=size_computing_menu_taille_grille(window)
+
+    window.blit(background,background_pos)
+    window.blit(title_menu_taille_grille,title_menu_taille_grille_pos)
+    for i in range(14):
+        if i<7:
+            if i==0:
+                window.blit(text_taille_2,(text_taille_pos[0]+backgroundXY[0]*0.12*i,text_taille_pos[1]))
+            elif i==1:
+                window.blit(text_taille_3,(text_taille_pos[0]+backgroundXY[0]*0.12*i,text_taille_pos[1]))
+            elif i==2:
+                window.blit(text_taille_4,(text_taille_pos[0]+backgroundXY[0]*0.12*i,text_taille_pos[1]))
+            elif i==3:
+                window.blit(text_taille_5,(text_taille_pos[0]+backgroundXY[0]*0.12*i,text_taille_pos[1]))
+            elif i==4:
+                window.blit(text_taille_6,(text_taille_pos[0]+backgroundXY[0]*0.12*i,text_taille_pos[1]))
+            elif i==5:
+                window.blit(text_taille_7,(text_taille_pos[0]+backgroundXY[0]*0.12*i,text_taille_pos[1]))
+            elif i==6:
+                window.blit(text_taille_8,(text_taille_pos[0]+backgroundXY[0]*0.12*i,text_taille_pos[1]))
+        else:
+            print(i)
+            if i==7:
+                window.blit(text_taille_9,(text_taille_pos[0]+backgroundXY[0]*0.12*(i-7),text_taille_pos[1]+backgroundXY[1]*0.25))
+            elif i==8:
+                window.blit(text_taille_10,(text_taille_pos[0]+backgroundXY[0]*0.12*(i-7),text_taille_pos[1]+backgroundXY[1]*0.25))
+            elif i==9:
+                window.blit(text_taille_11,(text_taille_pos[0]+backgroundXY[0]*0.12*(i-7),text_taille_pos[1]+backgroundXY[1]*0.25))
+            elif i==10:
+                window.blit(text_taille_12,(text_taille_pos[0]+backgroundXY[0]*0.12*(i-7),text_taille_pos[1]+backgroundXY[1]*0.25))
+            elif i==11:
+                window.blit(text_taille_13,(text_taille_pos[0]+backgroundXY[0]*0.12*(i-7),text_taille_pos[1]+backgroundXY[1]*0.25))
+            elif i==12:
+                window.blit(text_taille_14,(text_taille_pos[0]+backgroundXY[0]*0.12*(i-7),text_taille_pos[1]+backgroundXY[1]*0.25))
+            elif i==13:
+                window.blit(text_taille_15,(text_taille_pos[0]+backgroundXY[0]*0.12*(i-7),text_taille_pos[1]+backgroundXY[1]*0.25))
+
+    pygame.display.flip()
+    return window
+
+
+def action(etape,event):
+    if etape==0:
+        if event.type==pygame.MOUSEBUTTONDOWN:
+            background_mode_jeuXY,background_mode_jeu,background,title,icon_mode_jeu_1,icon_mode_jeu_2,text_mode_jeu_1,text_mode_jeu_2,text_createurs,background_pos,title_pos,background_mode_jeu_1_pos,background_mode_jeu_2_pos,icon_mode_jeu_1_pos,icon_mode_jeu_2_pos,text_mode_jeu_1_pos,text_mode_jeu_2_pos,text_createurs_pos=size_computing_menu_mode_jeu(window)
+            if background_mode_jeu_1_pos[0]<pygame.mouse.get_pos()[0]<background_mode_jeu_1_pos[0]+background_mode_jeuXY[0] and background_mode_jeu_1_pos[1]<pygame.mouse.get_pos()[1]<background_mode_jeu_1_pos[1]+background_mode_jeuXY[1]:
+                etape=1  
+            elif background_mode_jeu_2_pos[0]<pygame.mouse.get_pos()[0]<background_mode_jeu_2_pos[0]+background_mode_jeuXY[0] and background_mode_jeu_2_pos[1]<pygame.mouse.get_pos()[1]<background_mode_jeu_2_pos[1]+background_mode_jeuXY[1]:
+                etape=3
+    elif etape==1:
+        pass
+        
+    elif etape==2:
+        pass
+
+    elif etape==3:
+        pass
+
+    elif etape==4:
+        pass
+    
+    return etape
+
+
+# ____________________________________________________________
+# Calcule de la taille initiale de la fenetre
+window=pygame.display.set_mode()
+windowXY=window.get_size()
+windowXY=(windowXY[0]*0.8,windowXY[1]*0.8)
+window=pygame.display.set_mode((windowXY),pygame.RESIZABLE)
+# ____________________________________________________________
+# Création des fonts
+title_font=pygame.font.SysFont(None,300,False)
+# ____________________________________________________________
+# Chargement des images
+background_image=pygame.image.load("img/background.jpg")
+victory_banner_image=pygame.image.load("img/victoire.png")
+etoile_image=pygame.image.load("img/etoile.png")
+porte_image=pygame.image.load("img/porte.png")
+sous_marin_image=pygame.image.load("img/sousmarin.png")
+    
+# ____________________________________________________________
+
+
+
+
+
+
+etape,run=0,True
+window=affichage(window,etape)
+while run:
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            run=False
+        elif event.type==pygame.KEYDOWN or event.type==pygame.MOUSEBUTTONDOWN:
+            etape=action(etape,event)
+            print("Action de l'utilisateur")
+        elif event.type==pygame.VIDEORESIZE or event.type==pygame.VIDEOEXPOSE:
+            print("Window size changed")
+    window=affichage(window,etape)
+pygame.quit()
